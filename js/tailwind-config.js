bdbfea7cdeabfa2ebf41180e531e85f6013f077e
Mahmoud Amin <mahmoudamin20245@gmail.com>
2026-03-10 16:44:12 +0200

    first

---
+++ js/tailwind-config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+   darkMode: "class",
+   theme: {
+      extend: {
+         colors: {
+            primary: "#ccff00",
+            "background-light": "#F9F9F7",
+            "background-dark": "#1A1A1A",
+         },
+         fontFamily: {
+            display: ["Newsreader", "serif"],
+         },
+         borderRadius: {
+            DEFAULT: "0.25rem",
+            lg: "0.5rem",
+            xl: "0.75rem",
+            full: "9999px",
+         },
+      },
+   },
+};
